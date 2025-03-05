1bd93c47e5c5ecb239c60f9e90f2a81fd4085c35
Josh Sebastián López Murcia <65347220+joshmessi10@users.noreply.github.com>
2025-03-05 20:59:51 +0000

    Add files via upload

--- Punto5/EvalVisitor.py
+++ Punto5/EvalVisitor.py
@@ -30,6 +30,40 @@ class EvalVisitor(LabeledExprVisitor):
 	    return complexVal
 	def visitImagine(self,ctx):
 		return complex(float(ctx.REAL().getText()), ctx.COMPLEX().getText())
+	def visitId(self, ctx):
+		id = ctx.ID().getText()
+		return self.memory.get(id, None)
+	def visitMulDiv(self, ctx):
+		left = self.visit(ctx.expr(0))
+		right = self.visit(ctx.expr(1))
+		if ctx.op.type == LabeledExprParser.MUL:
+			return left * right
+		else:
+			if right == 0:
+				print("Error: Division por cero")
+				return None
+			else:
+				return left / right
+	def visitAddSub(self,ctx):
+		left = self.visit(ctx.expr(0))
+		right = self.visit(ctx.expr(1))
+		if right == LabeledExprParser.COMPLEX:
+		    return complex(left, right)
+		if ctx.op.type == LabeledExprParser.ADD:
+			return round(left + right, 10)
+		return round(left - right, 10)
+	def visitParens(self, ctx):
+	    text = ctx.getText()
+	    if not (text.startswith("(") and text.endswith(")")):
+	        print("Error: Parentesis no cerrados correctamente")
+	        return None
+	    return self.visit(ctx.expr())
+	def visitNeg(self,ctx):
+		value = self.visit(ctx.expr())
+		return -value if value is not None else None
+	def visitPos(self,ctx):
+	    value = self.visit(ctx.expr())
+	    return value if value is not None else None
 	
 	
 
